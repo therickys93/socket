69b3f8177082e0614980237d7157d9db94c87188
Riccardo Crippa <therickys93@gmail.com>
2015-12-15 10:46:30 +0100

    Added the c part

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # socket
-Socket made in java.
+Socket made in java and in c.
 
 I made this code in university
+
+Added the c part <br />
+<br />
+This code is in development so there could be some errors or bugs
---
+++ c/nslookup.c
@@ -0,0 +1,54 @@
+/**
+ * nslookup.c
+ *
+ * Riccardo Crippa
+ * therickys93@gmail.com
+ *
+ * Insert here your comments
+ */
+ 
+ #include <netdb.h>
+ #include <arpa/inet.h>
+ #include <stdio.h>
+ #include <string.h>
+ 
+ int main(int argc, char **argv)
+ {
+	 u_long address;
+	 if(argc != 3)
+	 {
+		 printf("Error: number of parameters incorrect\n");
+		 return 1;
+	 }
+	 else if(strcmp(argv[1], "s") == 0)
+	 {
+		 char *hostname = argv[2];
+		 struct hostent* hostinfo;
+		 hostinfo = gethostbyname(hostname);
+		 if(hostinfo == NULL)
+		 {
+			 printf("Error: No host found with that name\n");
+			 return 2; 
+		 }
+		 struct in_addr castDot;
+		 castDot.s_addr = *((u_long *) (hostinfo->h_addr_list[0]));
+		 printf("address: %u\n", castDot.s_addr);
+	 }
+	 else if(strcmp(argv[1], "d") == 0)
+	 {
+		 address = inet_addr(argv[1]);
+		 if(address == INADDR_NONE)
+		 {
+			 printf("Error: inet_address\n");
+			 return 3;
+		 }
+		 printf("address: %lu\n", address);
+	 }
+	 else
+	 {
+		 printf("usage: %s s [www.example.com]\n", argv[0]);
+		 printf("usage: %s d [192.168.1.4]\n", argv[0]);
+		 return 4;
+	 }
+	 return 0;
+ }
